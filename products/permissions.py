54938a97174edea5a1bfe209ec34e1bcb660917e
oswald15 <oswaldfankem15@gmail.com>
2026-09-07 18:54:45 +0200

    next

--- products/permissions.py
+++ products/permissions.py
@@ -6,7 +6,7 @@ class IsAdminOrApprovisionneur(permissions.BasePermission):
 
 class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'admin'
+        return request.user.role in ['admin', 'superadmin']
 
 class IsAdminOrSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
